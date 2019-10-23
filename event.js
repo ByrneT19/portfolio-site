a2139ede4f9513fcd71574d1fce28d1d37c54893
Tam Byrne <mercadier122@gmail.com>
2019-10-23 01:28:31 +0100

    new html

---
+++ event.js
@@ -0,0 +1,8 @@
+function bio() {
+    $('.bio-p').click('click', function() {
+        event.preventDefault;
+        $(this).closest('a').html(`
+        <p>aaaaaaaaaaaaaaaaaa</a>
+        `)
+    })
+}
