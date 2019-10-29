d822352b861f2247bfdf9eb3844674f805596586
Tam Byrne <mercadier122@gmail.com>
2019-10-29 14:00:39 +0000

    updated html, css and flexbox

--- event.js
+++ event.js
@@ -1,8 +1,2 @@
-function bio() {
-    $('.bio-p').click('click', function() {
-        event.preventDefault;
-        $(this).closest('a').html(`
-        <p>aaaaaaaaaaaaaaaaaa</a>
-        `)
-    })
-}
+
+
