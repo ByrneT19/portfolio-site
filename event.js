7c272b552445f2b8f19769cc68eae8f2441714c8
Tam Byrne <mercadier122@gmail.com>
2019-11-02 15:12:06 +0000

    media queries

--- event.js
+++ event.js
@@ -1,2 +1,30 @@
+projects = [
+    {
+    loc: <a href="https://github.com/ByrneT19/Quiz_App.git" target="_blank">Quiz App</a>,
+    thumb: images/HK1.jpg
+}
+];
 
+function loader() {
+    $('.projects').on('click', function(event) {
+        event.preventdefault();
+        $('.locs').html
+        (`${PROJECTS.loc},
+        <img class="thumbs" 'src', ${PROJECTS.images/thumb}>`)
+    })
+}
 
+$(window).on('scroll', function navScroll() {
+    if($(window).scrollTop()) {
+        $('nav').addClass('.stick');
+    } else {
+        $('nav');
+    }
+})
+    
+
+function loadPage() {
+    navScroll();
+}
+
+$(loadPage);
---
+++ readMe.md
@@ -0,0 +1,1 @@
+Check max-width of bio
