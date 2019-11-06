785c2690fc199701cc85c361a5d0cf90cc1aaaf5
Tam Byrne <mercadier122@gmail.com>
2019-11-06 00:08:47 +0000

    css

--- event.js
+++ event.js
@@ -1,30 +1,6 @@
-projects = [
-    {
-    loc: <a href="https://github.com/ByrneT19/Quiz_App.git" target="_blank">Quiz App</a>,
-    thumb: images/HK1.jpg
-}
-];
-
-function loader() {
-    $('.projects').on('click', function(event) {
-        event.preventdefault();
-        $('.locs').html
-        (`${PROJECTS.loc},
-        <img class="thumbs" 'src', ${PROJECTS.images/thumb}>`)
-    })
-}
-
-$(window).on('scroll', function navScroll() {
-    if($(window).scrollTop()) {
-        $('nav').addClass('.stick');
-    } else {
-        $('nav');
-    }
+function menuShow() {
+$('.hanBaoBao').click(function () {
+    $('this').closest('ul').show();
 })
-    
+};
 
-function loadPage() {
-    navScroll();
-}
-
-$(loadPage);
