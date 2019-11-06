27f1fc50fa9a5c1c37cb11b9dbb8a0d9fb0bfe9e
Tam Byrne <mercadier122@gmail.com>
2019-11-06 22:13:22 +0000

    css and js

--- event.js
+++ event.js
@@ -1,17 +1,29 @@
 function menuShow() {
-    let x = document.getElementById('.alt-id');
-    if (x.className === '.alt-menu') {
+    let x = document.getElementById('alt-id');
+    if (x.className === 'alt-menu') {
         x.className += ' responsive';
     } else {
-        x.className = '.alt-menu';
+        x.className = 'alt-menu';
     }
 };
 
-/*function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }*/
+function menuClick() {
+  $('this').on('click', function() {
+    $('.hanBaoBao').hide();
+  })
+}
+
+function menuItemsHide() {
+  $('.proLink', '.aboutLink').on('click', function() {
+    $('this').hide();
+    $('.hanBaoBao').show();
+  })
+}
+
+function pageLoad() {
+  menuShow();
+  menuClick();
+  menuItemsHide();
+}
+
+$(pageLoad);
